3397061585cb3472c42a6450deac7efd41097f3e
StanGlazenburg <48719495+StanGlazenburg@users.noreply.github.com>
2023-04-17 20:35:46 +0200

    Policies toegevoegd

---
+++ app/Policies/QuestionPolicy.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class QuestionPolicy
+{
+    use HandlesAuthorization;
+
+    public function before(User $user): bool|null
+    {
+        if ($user->is_admin) {
+            return true;
+        }
+
+        return null;
+    }
+
+    public function viewAny(): bool
+    {
+        return false;
+    }
+}
