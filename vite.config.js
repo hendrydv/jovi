e24d7b68622e3384483d2fbc330d6555a843abc2
Hendry de Vries <hendry.de.vries@kingbee.nl>
2023-04-24 17:29:50 +0200

    Fix images

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,6 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/images/jovi.png',
-                'resources/images/jovi_background.jpg',
-                'resources/images/jovi_favicon.png',
             ],
             refresh: true,
         }),
