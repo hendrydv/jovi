5bf7fd9fd3d4ba5ce05ca43acc17cfef287ae4a1
Stan_Glazenburg <stan.glazenburg@student.nhlstenden.com>
2023-06-08 11:49:22 +0200

    JOVI-62
    Dashboard geladen vanuit resultaten tabel

--- app/Filament/Widgets/InspectionsOverview.php
+++ app/Filament/Widgets/InspectionsOverview.php
@@ -3,6 +3,8 @@
 namespace App\Filament\Widgets;
 
 use App\Models\Inspection;
+use App\Models\InspectionMachineResult;
+use App\Models\SpaceMachine;
 use Filament\Widgets\Widget;
 
 class InspectionsOverview extends Widget
@@ -11,6 +13,8 @@ class InspectionsOverview extends Widget
     protected int | string | array $columnSpan = 'full';
 
     public $inspectionMachines = [];
+
+    public $machines = [];
 
     public function mount(): void
     {
@@ -22,24 +26,32 @@ class InspectionsOverview extends Widget
                 $this->inspectionMachines[$customer->name] = [];
 
                 $locations = $inspection->customer->locations;
-
-                $locations->each(function($location) use ($customer) {
+                $locations->each(function($location) use ($customer, $inspection) {
                     $this->inspectionMachines[$customer->name][$location->fullAddress()] = [];
 
                     $departments = $location->departments;
+                    $departments->each(function($department) use ($location, $customer, $inspection) {
 
-                        $departments->each(function($department) use ($location, $customer) {
+                        $this->inspectionMachines[$customer->name][$location->fullAddress()][$department->name] = [];
 
-                            $this->inspectionMachines[$customer->name][$location->fullAddress()][$department->name] = [];
+                        $spaces = $department->spaces;
+                        $spaces->each(function($space) use ($location, $department, $customer, $inspection) {
 
-                            $spaces = $department->spaces;
+                            $inspection_machine_results = InspectionMachineResult::query()
+                                ->select('space_machine_id')
+                                ->where(['inspection_id' => $inspection->id, 'space_machines.space_id' => $space->id])
+                                ->distinct()
+                                ->join('space_machines', 'inspection_machine_results.space_machine_id', '=', 'space_machines.id')
+                                ->get();
 
-                                $spaces->each(function($space) use ($location, $department, $customer) {
-
-                                    $machines = $space->machines;
-                                    $this->inspectionMachines[$customer->name][$location->fullAddress()][$department->name][$space->name] = $machines;
-                                });
+                            $inspection_machine_results->each(function($inspection_machine_result) use ($inspection, $space, $location, $department, $customer) {
+                                $space_machine = SpaceMachine::find($inspection_machine_result->space_machine_id);
+                                $this->machines[] = $space_machine->machine;
+                            });
+                            $this->inspectionMachines[$customer->name][$location->fullAddress()][$department->name][$space->name] = $this->machines;
+                            $this->machines = [];
                         });
+                    });
                 });
             });
     }
