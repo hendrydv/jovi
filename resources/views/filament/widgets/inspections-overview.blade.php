582f03b9750e8f80e50b07287dc0f7871ea0b505
Hendry de Vries <hendry.de.vries@kingbee.nl>
2023-06-12 13:30:11 +0200

    Add table

--- resources/views/filament/widgets/inspections-overview.blade.php
+++ resources/views/filament/widgets/inspections-overview.blade.php
@@ -13,19 +13,23 @@
                                                 @foreach($spaces as $space => $machines)
                                                     <x-accordion.item :name="$space" :first="$loop->first">
                                                         <x-accordion.list>
-                                                            <table>
+                                                            <table class="table-auto w-full border-collapse">
+                                                                <thead>
+                                                                    <tr class="bg-gray-500/5">
+                                                                        <th class="border text-start px-4 py-2">Machine</th>
+                                                                        <th class="border text-start px-4 py-2">Status</th>
+                                                                        <th class="border text-start px-4 py-2">Actie</th>
+                                                                    </tr>
+                                                                </thead>
                                                                 <tbody>
                                                                     @foreach($machines as $machine)
                                                                         <tr>
-                                                                            <td class="pl-6">{{ $machine['machine'] }}</td>
-                                                                            <td class="pl-5">{{$machine['state']}}</td>
-                                                                            <td class="pl-5">
-                                                                                <a href="/inspecties/{{$inspection->id}}/uitvoeren/{{$machine['space_machine_id']}}">
-                                                                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                                                                                        <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z" />
-                                                                                        <path fill-rule="evenodd" d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm9.707 5.707a1 1 0 00-1.414-1.414L9 12.586l-1.293-1.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
-                                                                                    </svg>
-                                                                                </a>
+                                                                            <td class="border px-4 py-3">{{ $machine['machine'] }}</td>
+                                                                            <td class="border px-4 py-3">{{$machine['state']}}</td>
+                                                                            <td class="border px-4 py-3">
+                                                                                <x-filament::link icon="heroicon-s-pencil" href="/inspecties/{{$inspection->id}}/uitvoeren/{{$machine['space_machine_id']}}">
+                                                                                    Uitvoeren
+                                                                                </x-filament::link>
                                                                             </td>
                                                                         </tr>
                                                                     @endforeach
