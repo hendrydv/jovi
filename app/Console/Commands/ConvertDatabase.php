ff7184f351256448469d1eb1278da3a198a05a1c
Hendry de Vries <hendry.de.vries@kingbee.nl>
2023-06-12 16:34:50 +0200

    Fix command

--- app/Console/Commands/ConvertDatabase.php
+++ app/Console/Commands/ConvertDatabase.php
@@ -37,19 +37,27 @@ class ConvertDatabase extends Command
      */
     public function handle(): int
     {
+        $this->call('migrate:fresh', [
+            '--force' => true,
+        ]);
+
         DB::beginTransaction();
 
-        $user = new User();
-        $user->name = 'Admin Admin';
-        $user->email = 'admin@admin.com';
-        $user->password = bcrypt('admin123');
-        $user->is_admin = true;
-        $user->save();
-
         try {
+            $user = new User();
+            $user->name = 'Admin Admin';
+            $user->email = 'admin@admin.com';
+            $user->password = bcrypt('admin123');
+            $user->is_admin = true;
+            $user->save();
+
+            $this->info('Admin user created');
+
             $db = DB::connection('mysql_acces');
 
             $relaties = $db->select('select * from relaties');
+
+            $this->info("Relaties: " . count($relaties) . " gevonden");
 
             foreach ($relaties as $relatie) {
                 $customer = new Customer();
@@ -61,33 +69,63 @@ class ConvertDatabase extends Command
                 $customer->preferred_month = array_search($relatie->Voorkeursmaand, Customer::MONTHS) ?: null;
                 $customer->notes = $relatie->Memo;
                 $customer->save();
+
+                $adressen = $db->select('select * from adressen where Relatienummer = ?', [$relatie->Relatienummer]);
+
+                $customerName = $relatie->Naam;
+                $this->info("$customerName: Adressen: " . count($adressen) . " gevonden");
+
+                foreach ($adressen as $adres) {
+                    $location = new Location();
+                    $location->id = $adres->AdresPrimary;
+                    $location->customer_id = $adres->Relatienummer;
+                    $location->street = $adres->Adres;
+                    $location->city = $adres->Plaats;
+                    $location->save();
+
+                    $ruimtes = $db->select('select * from ruimtes where Adres = ?', [$adres->Adres]);
+
+                    $departmentIds = [];
+                    foreach ($ruimtes as $ruimte) {
+                        $departmentIds[] = $ruimte->Afdelingsnummer;
+                    }
+
+                    $afdelingen = $db->select('select * from Afdelingen where Afdelingsnummer in (' . implode(',', array_unique($departmentIds)) . ')');
+
+                    $adresName = $relatie->Naam . " " . $adres->Adres;
+                    $this->info("$adresName: Afdelingen: " . count($afdelingen) . " gevonden");
+
+                    foreach ($afdelingen as $afdeling) {
+                        DB::table('departments')->insertOrIgnore([
+                            'id' => $afdeling->Afdelingsnummer,
+                            'location_id' => $adres->AdresPrimary,
+                            'name' => $afdeling->Afdelingsnaam,
+                            'created_at' => date('Y-m-d H:i:s'),
+                            'updated_at' => date('Y-m-d H:i:s'),
+                        ]);
+
+                        $ruimtes = $db->select('select * from ruimtes where Afdelingsnummer = ?', [$afdeling->Afdelingsnummer]);
+
+                        $afdelingName = $relatie->Naam . " " . $adres->Adres . " " . $afdeling->Afdelingsnaam;
+                        $this->info("$afdelingName: Ruimtes: " . count($ruimtes) . " gevonden");
+
+                        foreach ($ruimtes as $ruimte) {
+                            DB::table('spaces')->insertOrIgnore([
+                                'id' => $ruimte->Ruimtenummer,
+                                'department_id' => $afdeling->Afdelingsnummer,
+                                'name' => $ruimte->Ruimtenaam,
+                                'created_at' => date('Y-m-d H:i:s'),
+                                'updated_at' => date('Y-m-d H:i:s'),
+                            ]);
+                        }
+                    }
+                }
+                $this->info("--------------------");
             }
-
-            $adressen = $db->select('select * from adressen');
-
-            foreach ($adressen as $adres){
-                $location = new Location();
-                $location->id = $adres->AdresPrimary;
-                $location->customer_id = $adres->Relatienummer;
-                $location->street = $adres->Adres;
-                $location->city = $adres->Plaats;
-                $location->save();
-            }
-
-//            $afdelingen = $db->select('select * from afdelingen left join ruimtes on afdelingen.Afdelingsnummer = ruimtes.Afdelingsnummer');
-//
-//            foreach ($afdelingen as $afdeling){
-//                $department = new Department();
-//                $department->id = $afdeling->Afdelingsnummer;
-//                $department->name = $afdeling->Afdelingsnaam;
-//                dd($afdeling->Adres);
-//                $department->location_id = $afdeling->Adress;
-//                $department->save();
-//            }
-
         } catch (Exception | ErrorException | QueryException $e) {
+            $this->info("Something went wrong: " . $e->getMessage());
             DB::rollBack();
-            throw $e;
+            return Command::FAILURE;
         }
 
         DB::commit();
