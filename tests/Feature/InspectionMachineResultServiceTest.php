c37ae36018410b9f6010d2163c418490fc96bf90
Hendry de Vries <hendry.de.vries@kingbee.nl>
2023-06-28 14:39:20 +0200

    Test

--- tests/Feature/InspectionMachineResultServiceTest.php
+++ tests/Feature/InspectionMachineResultServiceTest.php
@@ -17,7 +17,7 @@ class InspectionMachineResultServiceTest extends TestCase
 {
     use DatabaseTransactions;
 
-    public function testCreateResultsWithInspection()
+    public function testCreateMachineResultsWithInspection()
     {
         $customer = Customer::factory()->create();
         $inspection = Inspection::factory()->for($customer)->create();
@@ -41,7 +41,46 @@ class InspectionMachineResultServiceTest extends TestCase
             }
         }
 
+        InspectionMachineResultService::createResultsWithInspection($inspection);
+
+        $inspectionMachineResults = InspectionMachineResult::all();
+
+        $this->assertNotEmpty($inspectionMachineResults);
+
         $manyResults = 2 * 2 * 2 * 2 * 2;
+
+        $this->assertEquals($manyResults, $inspectionMachineResults->count());
+    }
+
+    public function testCreateTypeResultsWithInspection()
+    {
+        $customer = Customer::factory()->create();
+        $inspection = Inspection::factory()->for($customer)->create();
+        $locations = Location::factory()->for($customer)->count(2)->create();
+
+        $inspectionList = InspectionList::factory()->create();
+        $inspectionQuestions = Question::factory()->count(2)->create();
+        $inspectionList->questions()->saveMany($inspectionQuestions);
+
+        $inspection->inspectionType->inspectionList()->associate($inspectionList);
+
+        foreach ($locations as $location) {
+            $departments = Department::factory()->for($location)->count(2)->create();
+            foreach ($departments as $department) {
+                $spaces = Space::factory()->for($department)->count(2)->create();
+                foreach ($spaces as $space) {
+                    $inspectionList = InspectionList::factory()->create();
+                    $questions = Question::factory()->count(2)->create();
+                    $inspectionList->questions()->saveMany($questions);
+                    $machines = Machine::factory()->for($inspectionList)->count(2)->create();
+                    foreach ($machines as $idx => $machine) {
+                        $space->machines()->attach($machine, [
+                            'inventory_number' => $idx + 1,
+                        ]);
+                    }
+                }
+            }
+        }
 
         InspectionMachineResultService::createResultsWithInspection($inspection);
 
@@ -49,6 +88,14 @@ class InspectionMachineResultServiceTest extends TestCase
 
         $this->assertNotEmpty($inspectionMachineResults);
 
+        $manyResults = 2 * 2 * 2 * 2 * 2;
+
         $this->assertEquals($manyResults, $inspectionMachineResults->count());
+
+        $questionIds = $inspectionQuestions->pluck('id')->toArray();
+
+        foreach ($inspectionMachineResults as $inspectionMachineResult) {
+            $this->assertTrue(in_array($inspectionMachineResult->question->id, $questionIds));
+        }
     }
 }
